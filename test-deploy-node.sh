4702e46a01f33cb0b4258f829ff2e42fa7d3f8ee
Felix Serang <byte007@zoho.com>
2025-06-20 16:53:02 +0700

    ✨ feat: manually executing deploying geth node with customizing host, port, network type

---
+++ test-deploy-node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ./deploy-node.sh ip_address ubuntu ~/.ssh/id_rsa geth
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <node_ip> <user> <private_key_file> <node_type>"
+    exit 1
+fi
+
+EXTRA_VARS="GETH_HTTP_PORT=8545 GETH_WS_PORT=8546 GETH_P2P_PORT=30303 GETH_NETWORK=sepolia GETH_CONTAINER_NAME=geth-node-1"
+ansible-playbook -i "$1," --user $2 --private-key $3 --extra-vars "$EXTRA_VARS" _infra/ansible/playbook-$4.yml
